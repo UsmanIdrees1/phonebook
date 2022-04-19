f85abec150bf60832d58de0ef61acd2a4107882b
ItsHMS <hamidmehmood816@gmail.com>
2022-04-19 14:37:43 +0500

    modified forget password changed its layout using vuetify

--- src/views/ForgetPassword.vue
+++ src/views/ForgetPassword.vue
@@ -1,14 +1,22 @@
 <template>
-  <div>
-    <form @submit.prevent="submit">
-      <label for="email">Email</label>
-      <input type="email" v-model="email" />
-
-      <button value="submit">Submit</button>
-    </form>
-  </div>
+  <v-app>
+    <v-card width="400" class="mx-auto mt-5">
+      <v-toolbar dark color="primary">
+        <v-toolbar-title>Login form</v-toolbar-title>
+      </v-toolbar>
+      <v-card-text>
+        <v-form @submit.prevent="submit">
+          <v-text-field label="Email" prepend-icon="email" v-model="email" />
+          <v-divider></v-divider>
+          <v-card-actions>
+            <v-btn type="submit" color="info">Login</v-btn>
+            <v-btn text color="success" to="/">Register</v-btn>
+          </v-card-actions>
+        </v-form>
+      </v-card-text>
+    </v-card>
+  </v-app>
 </template>
-
 <script>
 import { getAuth, sendPasswordResetEmail } from "firebase/auth";
 export default {
