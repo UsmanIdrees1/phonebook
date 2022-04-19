a1829ea47eee7878957498b9e23ceb058fa816ef
ItsHMS <hamidmehmood816@gmail.com>
2022-04-19 14:31:22 +0500

    added vuetify added configuration for vuetify

--- src/main.js
+++ src/main.js
@@ -2,34 +2,34 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-// Import the functions you need from the SDKs you need
+import vuetify from "./plugins/vuetify";
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 Vue.config.productionTip = false;
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAj2lXu5b9Kd2oV9ZR5N62j-kiOCTM4UTo",
-  authDomain: "posts-web-project.firebaseapp.com",
-  databaseURL: "https://posts-web-project-default-rtdb.firebaseio.com",
-  projectId: "posts-web-project",
-  storageBucket: "posts-web-project.appspot.com",
-  messagingSenderId: "663846338457",
-  appId: "1:663846338457:web:e28b2b2d26d1b94fe3dd08",
+  apiKey: "AIzaSyA2Byr7tTvgQ2nCzbU0CGJgcK6PB2FfSzM",
+  authDomain: "vuejs-b6b7f.firebaseapp.com",
+  databaseURL: "https://vuejs-b6b7f-default-rtdb.firebaseio.com",
+  projectId: "vuejs-b6b7f",
+  storageBucket: "vuejs-b6b7f.appspot.com",
+  messagingSenderId: "523459296499",
+  appId: "1:523459296499:web:ddde9befdd18dd92d28b86",
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const db = getFirestore(app);
+console.log(db.type);
 onAuthStateChanged(auth, (user) => {
+  console.log(user.uid);
   store.dispatch("user_auth", user);
   new Vue({
     router,
     store,
+    vuetify,
     render: (h) => h(App),
   }).$mount("#app");
 });
